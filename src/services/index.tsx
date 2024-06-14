6d43c0d20b8d411f0a9eb23451b54265d0353d7f
MICHELLE CAROLINE MARTINS DOS SANTOS <michelle.cmsantos1@senacsp.edu.br>
2024-06-14 19:43:33 -0300

    finalizando segunda tela

---
+++ src/services/index.tsx
@@ -0,0 +1,11 @@
+import * as ingredients from './ingredientsServices'
+import * as recipes from './recipesServices'
+
+export const services = {
+    ingredients,
+    recipes,
+
+    storage:{
+        imagePath: "https://zcspuupbusjskptoupoa.supabase.co/storage/v1/object/public/ingredients"
+    }
+}
---
+++ src/services/service.types.d.ts
@@ -0,0 +1,12 @@
+type IngredientResponse ={
+    id: string
+    name: string
+    image: string
+}
+
+type RecipeResponse = {
+    id: string
+    name: string
+    minutes: number
+    image: string
+}
